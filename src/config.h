d996814230d5113ca5ba7a603e09a5ab0077ac90
Caivas, David <David.Caivas@firma.seznam.cz>
2019-05-24 20:24:46 +0200

    config.h update

--- src/config.h
+++ src/config.h
@@ -1,5 +1,12 @@
 // wanna debug?
 #define DEBUG true
 
-// pin definitions
+// PIN DEFINITIONS
+// ---------------
+
 #define ADC_SEED_PIN A0
+
+//VFD display Pin config
+#define VFD_SCLK_PIN 2
+#define VFD_RST_PIN  3
+#define VFD_DATA_PIN 4
