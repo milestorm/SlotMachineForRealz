7982f0b7bf1c2e53ad6378c7542683445fe43b30
David Caivas <david.caivas@firma.seznam.cz>
2024-06-21 19:35:43 +0200

    added all three reels

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@
 #include "vfdcontrol.h"
 #include "tools.h"
 
-
-OneButton startButton(4, true);
+// REELS DEFINITIONS
+// ---------------------------------
 
 // Define the constants for the win amounts for different symbols
 // Bell, Plum, Orange, Lemon and Cherry payout is the same even in higher multiwin bets
@@ -28,21 +28,55 @@ int reelSymbols2[24] = {8,3,3,3,5,3,2,2,2,1,1,7,1,1,1,6,6,1,0,0,0,0,4,4};
 int reelSymbols3[24] = {8,2,0,0,0,5,5,3,3,3,7,6,1,1,1,0,1,0,0,4,4,2,2,2};
 int reelLength = 24;
 
+const long flashOnTime = 400;
+const long flashOffTime = 300;
+
+// REEL 1
+const int sensorPin1 = REEL_1__IR_SENSOR;
+AccelStepper stepper1(AccelStepper::FULL4WIRE, REEL_1__MOTOR_A, REEL_1__MOTOR_B, REEL_1__MOTOR_C, REEL_1__MOTOR_D);
+Flasher reel1_light1(REEL_1__LIGHT_1, flashOnTime, flashOffTime);
+Flasher reel1_light2(REEL_1__LIGHT_2, flashOnTime, flashOffTime);
+Flasher reel1_light3(REEL_1__LIGHT_3, flashOnTime, flashOffTime);
+Reel reel1(1, stepper1, reelSymbols1, reelLength, sensorPin1, reel1_light1, reel1_light2, reel1_light3);
+// REEL 2
+const int sensorPin2 = REEL_2__IR_SENSOR;
+AccelStepper stepper2(AccelStepper::FULL4WIRE, REEL_2__MOTOR_A, REEL_2__MOTOR_B, REEL_2__MOTOR_C, REEL_2__MOTOR_D);
+Flasher reel2_light1(REEL_2__LIGHT_1, flashOnTime, flashOffTime);
+Flasher reel2_light2(REEL_2__LIGHT_2, flashOnTime, flashOffTime);
+Flasher reel2_light3(REEL_2__LIGHT_3, flashOnTime, flashOffTime);
+Reel reel2(2, stepper2, reelSymbols2, reelLength, sensorPin2, reel2_light1, reel2_light2, reel2_light3);
+// REEL 3
+const int sensorPin3 = REEL_3__IR_SENSOR;
+AccelStepper stepper3(AccelStepper::FULL4WIRE, REEL_3__MOTOR_A, REEL_3__MOTOR_B, REEL_3__MOTOR_C, REEL_3__MOTOR_D);
+Flasher reel3_light1(REEL_3__LIGHT_1, flashOnTime, flashOffTime);
+Flasher reel3_light2(REEL_3__LIGHT_2, flashOnTime, flashOffTime);
+Flasher reel3_light3(REEL_3__LIGHT_3, flashOnTime, flashOffTime);
+Reel reel3(3, stepper3, reelSymbols3, reelLength, sensorPin3, reel3_light1, reel3_light2, reel3_light3);
+
+// BUTTONS DEFINITIONS
+// ---------------------------------
+
+OneButton startButton(BUTT_START, true);
 
 
-// bool textWritten = false;
-
-const byte sensorPin = 2; // please define this pin in config.h
-
-
-AccelStepper stepper(AccelStepper::FULL4WIRE, 8, 9, 10, 11);
-Flasher reel2_bulb1(5, 400, 300);
-Flasher reel2_bulb2(6, 400, 300);
-Flasher reel2_bulb3(7, 400, 300);
-Reel reel2(1, stepper, reelSymbols2, reelLength, sensorPin, reel2_bulb1, reel2_bulb2, reel2_bulb3);
+// DISPLAY DEFINITIONS
+// ---------------------------------
 
 // Samsung_16LF01_VFD vfdisplay(VFD_SCLK_PIN, VFD_DATA_PIN, VFD_RST_PIN);
 // Vfdcontrol vfd(vfdisplay);
+
+
+// HELPERS
+// ---------------------
+uint16_t targetValueReel1;
+uint16_t targetValueReel2;
+uint16_t targetValueReel3;
+
+uint16_t* futureSymbols1;
+uint16_t* futureSymbols2;
+uint16_t* futureSymbols3;
+
+// bool textWritten = false;
 
 /**
  * Calculates winning amount
@@ -66,20 +100,38 @@ int calculateWinAmount(int symbol, int standardBet, int multiwinBet) {
 
 void startButtonFn() {
 	// TODO: kdyz se reel toci, tak nemoznost zmacknout button
-	uint16_t targetValueReel2 = Entropy.random(96, 160);
+	targetValueReel1 = Entropy.random(96, 160);
+	targetValueReel2 = Entropy.random(2, 72);
+	targetValueReel3 = Entropy.random(2, 48);
 
-	uint16_t* futureSymbols = reel2.getSymbolsAfterSpin(targetValueReel2);
+	targetValueReel2 = targetValueReel1 + targetValueReel2;
+	targetValueReel3 = targetValueReel2 + targetValueReel3;
 
-	Serial.print("futureSymbols: ");
-	Serial.print(futureSymbols[0]);
-	Serial.print(", ");
-	Serial.print(futureSymbols[1]);
-	Serial.print(", ");
-	Serial.println(futureSymbols[2]);
+	futureSymbols1 = reel1.getSymbolsAfterSpin(targetValueReel1);
+	futureSymbols2 = reel2.getSymbolsAfterSpin(targetValueReel2);
+	futureSymbols3 = reel3.getSymbolsAfterSpin(targetValueReel3);
 
+	Serial.println("futureSymbols:");
+	Serial.print(futureSymbols1[0]);
+	Serial.print(" - ");
+	Serial.print(futureSymbols2[0]);
+	Serial.print(" - ");
+	Serial.println(futureSymbols3[0]);
+	Serial.print(futureSymbols1[1]);
+	Serial.print(" - ");
+	Serial.print(futureSymbols2[1]);
+	Serial.print(" - ");
+	Serial.println(futureSymbols3[1]);
+	Serial.print(futureSymbols1[2]);
+	Serial.print(" - ");
+	Serial.print(futureSymbols2[2]);
+	Serial.print(" - ");
+	Serial.println(futureSymbols3[2]);
+
+	reel1.spin(targetValueReel1);
 	reel2.spin(targetValueReel2);
+	reel3.spin(targetValueReel3);
 	// return symbols are at reel2.reelWinSymbols
-
 }
 
 /*
@@ -104,7 +156,9 @@ void setup() {
 
 	// vfd.init();
 
+	reel1.init();
 	reel2.init();
+	reel3.init();
 
 	startButton.attachClick(startButtonFn);
 
@@ -115,7 +169,9 @@ void loop() {
 
 	// vfd.tick();
 
+	reel1.tick();
 	reel2.tick();
+	reel3.tick();
 	// reelWatcher();
 
 	// vfd.print(millis() / 1000);
