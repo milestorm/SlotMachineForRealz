c1cca991e2d4c5f7d42c688833036a55034b1a5a
David Caivas <david.caivas@firma.seznam.cz>
2024-06-21 18:01:53 +0200

    pinout

--- src/config.h
+++ src/config.h
@@ -7,44 +7,44 @@
 #define ADC_SEED_PIN A0
 
 //VFD display Pin config
-#define VFD_SCLK_PIN 2
-#define VFD_RST_PIN  3
-#define VFD_DATA_PIN 4
+#define VFD_SCLK_PIN 10
+#define VFD_RST_PIN  12
+#define VFD_DATA_PIN 14
 
 // REEL 1
-#define REEL_1__MOTOR_A 5
-#define REEL_1__MOTOR_B 6
+#define REEL_1__MOTOR_A 3
+#define REEL_1__MOTOR_B 5
 #define REEL_1__MOTOR_C 7
-#define REEL_1__MOTOR_D 8
-#define REEL_1__IR_SENSOR 9
-#define REEL_1__LIGHT_1 10
-#define REEL_1__LIGHT_2 11
-#define REEL_1__LIGHT_3 12
+#define REEL_1__MOTOR_D 9
+#define REEL_1__IR_SENSOR 2
+#define REEL_1__LIGHT_1 4
+#define REEL_1__LIGHT_2 6
+#define REEL_1__LIGHT_3 8
 
 // REEL 2
-#define REEL_2__MOTOR_A 13
-#define REEL_2__MOTOR_B 14
-#define REEL_2__MOTOR_C 15
-#define REEL_2__MOTOR_D 16
-#define REEL_2__IR_SENSOR 17
+#define REEL_2__MOTOR_A 17
+#define REEL_2__MOTOR_B 19
+#define REEL_2__MOTOR_C 21
+#define REEL_2__MOTOR_D 23
+#define REEL_2__IR_SENSOR 16
 #define REEL_2__LIGHT_1 18
-#define REEL_2__LIGHT_2 19
-#define REEL_2__LIGHT_3 20
+#define REEL_2__LIGHT_2 20
+#define REEL_2__LIGHT_3 22
 
 // REEL 3
-#define REEL_3__MOTOR_A 21
-#define REEL_3__MOTOR_B 22
-#define REEL_3__MOTOR_C 23
-#define REEL_3__MOTOR_D 24
-#define REEL_3__IR_SENSOR 25
+#define REEL_3__MOTOR_A 25
+#define REEL_3__MOTOR_B 27
+#define REEL_3__MOTOR_C 29
+#define REEL_3__MOTOR_D 31
+#define REEL_3__IR_SENSOR 24
 #define REEL_3__LIGHT_1 26
-#define REEL_3__LIGHT_2 27
-#define REEL_3__LIGHT_3 28
+#define REEL_3__LIGHT_2 28
+#define REEL_3__LIGHT_3 30
 
 // BUTTONS
-#define BUTT_START 29
-#define BUTT_AUTO_START 30
-#define BUTT_MAX_BET 31
-#define BUTT_BET 32
-#define BUTT_RISK 33
-#define BUTT_TAKE_FROM_MULTIWIN 34
+#define BUTT_START 11
+#define BUTT_BET 15
+#define BUTT_MAX_BET 32
+#define BUTT_AUTO_START 33
+#define BUTT_RISK 34
+#define BUTT_TAKE_FROM_MULTIWIN 35
